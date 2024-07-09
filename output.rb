f019470a685260b04b2f9b564b5a7ef5f7d32353
Felipe Hnessler Koetz <felipehenssler@gmail.com>
2024-07-09 15:05:39 -0300

    Refactor code to remove unnecessary if-else statements and unused variables

--- output.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-if(true)
-  peter = "parker"
-elsif(false)
-
-elsif(false)
-  
-end
--- parser.rb
+++ parser.rb
@@ -24,7 +24,7 @@ class Parser < Rly::Yacc
   end
 
   rule 'statement : expression END_OF_STATEMENT' do |*args|
-    args[0].value = args[1].value
+    args[0].value = "#{args[1].value}\n"
   end
 
   rule 'expression : INTEGER
@@ -35,7 +35,7 @@ class Parser < Rly::Yacc
   end
 
   rule 'expression : BREAK' do |*args|
-    args[0].value = "break\n"
+    args[0].value = 'break'
   end
 
   rule 'expression : STRING' do |*args|
@@ -78,7 +78,7 @@ class Parser < Rly::Yacc
                    | IDENTIFIER MODULUS_ASSIGN expression
                    | IDENTIFIER EXPONENT_ASSIGN expression
                    | IDENTIFIER ASSIGN expression' do |*args|
-    args[0].value = "#{args[1].value} #{args[2].value} #{args[3].value}\n"
+    args[0].value = "#{args[1].value} #{args[2].value} #{args[3].value}"
   end
 
   rule 'expression : IDENTIFIER OPEN_PARENTHESIS expression_list CLOSE_PARENTHESIS' do |*args|
@@ -98,70 +98,70 @@ class Parser < Rly::Yacc
   end
 
   rule 'expression : PRINT OPEN_PARENTHESIS expression CLOSE_PARENTHESIS' do |*args|
-    args[0].value = "puts #{args[3].value}\n"
+    args[0].value = "puts #{args[3].value}"
   end
 
   rule 'expression : RETURN OPEN_PARENTHESIS expression CLOSE_PARENTHESIS' do |*args|
-    args[0].value = "return #{args[3].value}\n"
+    args[0].value = "return #{args[3].value}"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "if #{args[3].value}\nend\n"
+    args[0].value = "if #{args[3].value}\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     if_block = add_identation(args[6])
-    args[0].value = "if #{args[3].value}\n#{if_block}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSE EMPTY_BLOCK' do |*args|
-    args[0].value = "if #{args[3].value}\nelse\nend\n"
+    args[0].value = "if #{args[3].value}\nelse\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     if_block = add_identation(args[6])
     else_statement_list = add_identation(args[10])
-    args[0].value = "if #{args[3].value}\n#{if_block}else\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}else\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSE EMPTY_BLOCK' do |*args|
     if_block = add_identation(args[6])
-    args[0].value = "if #{args[3].value}\n#{if_block}else\nend\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}else\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     else_statement_list = add_identation(args[8])
-    args[0].value = "if #{args[3].value}\nelse\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\nelse\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
     if_block = add_identation(args[6])
-    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\nend\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     elsif_block = add_identation(args[11])
-    args[0].value = "if #{args[3].value}\nelsif #{args[8]}\n#{elsif_block}end\n"
+    args[0].value = "if #{args[3].value}\nelsif #{args[8]}\n#{elsif_block}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\nend\n"
+    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     if_block = add_identation(args[6])
     elsif_block = add_identation(args[13])
 
-    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10]}\n#{elsif_block}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10]}\n#{elsif_block}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET elsif_list' do |*args|
     if_block = add_identation(args[6])
-    args[0].value = "if #{args[3].value}\n#{if_block}#{args[8]}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}#{args[8]}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK elsif_list' do |*args|
-    args[0].value = "if #{args[3].value}\n#{args[6].value}end\n"
+    args[0].value = "if #{args[3].value}\n#{args[6].value}end"
   end
 
   rule 'elsif_list : ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
@@ -184,17 +184,17 @@ class Parser < Rly::Yacc
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSE EMPTY_BLOCK' do |*args|
     if_block = add_identation(args[6])
-    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\nelse\nend\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\nelse\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET  statement_list CLOSE_BRACKET ELSE EMPTY_BLOCK' do |*args|
     elsif_block = add_identation(args[11])
-    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\n#{elsif_block}else\nend\n"
+    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\n#{elsif_block}else\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     else_statement_list = add_identation(args[13])
-    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\nelse\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\nelse\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
@@ -202,104 +202,105 @@ class Parser < Rly::Yacc
     elsif_block = add_identation(args[13])
     else_statement_list = add_identation(args[17])
 
-    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10]}\n#{elsif_block}else\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10]}\n#{elsif_block}else\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSE EMPTY_BLOCK' do |*args|
-    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\nelse\nend\n"
+    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\nelse\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSE EMPTY_BLOCK' do |*args|
     if_block = add_identation(args[6])
     elsif_block = add_identation(args[13])
-    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\n#{elsif_block}else\nend\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\n#{elsif_block}else\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     if_block = add_identation(args[6])
     else_statement_list = add_identation(args[15])
-    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\nelse\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}elsif #{args[10].value}\nelse\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK ELSIF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     elsif_block = add_identation(args[11])
     else_statement_list = add_identation(args[15])
-    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\n#{elsif_block}else\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\nelsif #{args[8].value}\n#{elsif_block}else\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET elsif_list ELSE EMPTY_BLOCK' do |*args|
     if_block = add_identation(args[6])
-    args[0].value = "if #{args[3].value}\n#{if_block}#{args[8].value}else\nend\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}#{args[8].value}else\nend"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET elsif_list ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     if_block = add_identation(args[6])
     else_statement_list = add_identation(args[11])
-    args[0].value = "if #{args[3].value}\n#{if_block}#{args[8].value}else\n#{else_statement_list}end\n"
+    args[0].value = "if #{args[3].value}\n#{if_block}#{args[8].value}else\n#{else_statement_list}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK elsif_list ELSE OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     elsif_block = add_identation(args[9])
-    args[0].value = "if #{args[3].value}\n#{args[6].value}else\n#{elsif_block}end\n"
+    args[0].value = "if #{args[3].value}\n#{args[6].value}else\n#{elsif_block}end"
   end
 
   rule 'expression : IF OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK elsif_list ELSE EMPTY_BLOCK' do |*args|
-    args[0].value = "if #{args[3].value}\n#{args[6].value}else\nend\n"
+    args[0].value = "if #{args[3].value}\n#{args[6].value}else\nend"
   end
 
   rule 'expression : FUNCTION_DEFINITION IDENTIFIER OPEN_PARENTHESIS CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "def #{args[2].value}\nend\n"
+    args[0].value = "def #{args[2].value}\nend"
   end
 
   rule 'expression : FUNCTION_DEFINITION IDENTIFIER OPEN_PARENTHESIS CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     function_statement_list = add_identation(args[6])
 
-    args[0].value = "def #{args[2].value}\n#{function_statement_list}end\n"
+    args[0].value = "def #{args[2].value}\n#{function_statement_list}end"
   end
 
   rule 'expression : FUNCTION_DEFINITION IDENTIFIER OPEN_PARENTHESIS IDENTIFIER CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "def #{args[2].value}(#{args[4].value})\nend\n"
+    args[0].value = "def #{args[2].value}(#{args[4].value})\nend"
   end
 
   rule 'expression : FUNCTION_DEFINITION IDENTIFIER OPEN_PARENTHESIS IDENTIFIER CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     function_statement_list = add_identation(args[7])
-    args[0].value = "def #{args[2].value}(#{args[4].value})\n#{function_statement_list}end\n"
+    args[0].value = "def #{args[2].value}(#{args[4].value})\n#{function_statement_list}end"
   end
 
   rule 'expression : FUNCTION_DEFINITION IDENTIFIER OPEN_PARENTHESIS expression_list CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "def #{args[2].value}(#{args[4].value})\nend\n"
+    args[0].value = "def #{args[2].value}(#{args[4].value})\nend"
   end
 
   rule 'expression : FUNCTION_DEFINITION IDENTIFIER OPEN_PARENTHESIS expression_list CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     function_statement_list = add_identation(args[7])
-    args[0].value = "def #{args[2].value}(#{args[4].value})\n#{function_statement_list}end\n"
+    args[0].value = "def #{args[2].value}(#{args[4].value})\n#{function_statement_list}end"
   end
 
   rule 'expression : WHILE OPEN_PARENTHESIS expression CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "while #{args[3].value}\nend\n"
+    args[0].value = "while #{args[3].value}\nend"
   end
 
   rule 'expression : WHILE OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     while_statement_list = add_identation(args[6])
-    args[0].value = "while #{args[3].value}\n#{while_statement_list}end\n"
+    args[0].value = "while #{args[3].value}\n#{while_statement_list}end"
   end
 
   rule 'expression : FOR OPEN_PARENTHESIS INTEGER CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "#{args[3]}.times do\nend\n"
+    args[0].value = "#{args[3]}.times do\nend"
   end
 
   rule 'expression : FOR OPEN_PARENTHESIS INTEGER CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     for_statement_list = add_identation(args[6])
-    args[0].value = "#{args[3]}.times do\n#{for_statement_list}end\n"
+    args[0].value = "#{args[3]}.times do\n#{for_statement_list}end"
   end
 
+  # aceitar variavel no lugar do inteiro
   rule 'expression : FOR OPEN_PARENTHESIS INTEGER COMMA IDENTIFIER CLOSE_PARENTHESIS EMPTY_BLOCK' do |*args|
-    args[0].value = "#{args[3]}.times do |#{args[5].value}|\nend\n"
+    args[0].value = "#{args[3]}.times do |#{args[5].value}|\nend"
   end
 
   rule 'expression : FOR OPEN_PARENTHESIS INTEGER COMMA IDENTIFIER CLOSE_PARENTHESIS OPEN_BRACKET statement_list CLOSE_BRACKET' do |*args|
     for_statement_list = add_identation(args[8])
-    args[0].value = "#{args[3]}.times do |#{args[5].value}|\n#{for_statement_list}end\n"
+    args[0].value = "#{args[3]}.times do |#{args[5].value}|\n#{for_statement_list}end"
   end
 end
 # rubocop:enable Metrics/LineLength
